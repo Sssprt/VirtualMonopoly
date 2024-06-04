4ed92ae8936e952e2a769aceda9cc1535caae7c4
Ssprt <145202052+Sssprt@users.noreply.github.com>
2024-06-04 21:26:57 +0300

    Update Client.java

--- src/Client.java
+++ src/Client.java
@@ -2,13 +2,7 @@ public class Client {
     String name;
     int balance = 0;
 
-    public String getName(){
-        return this.name;
-    }
-    public void setName(String name){
-        this.name = name;
-    }
-    public void setBalance(int balance){
-        this.balance = balance;
-    }
+    public String getName(){return this.name;}
+    public void setName(String name){this.name = name;}
+    public void setBalance(int balance){this.balance = balance;}
 }
